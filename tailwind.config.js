e124e9bd1834d9892d836e30da0e3317f3d2530d
Dev in Lounge <rodrigo@rtoledo.inf.br>
2025-01-04 11:46:06 -0300

    Add EditorConfig and Prettier configuration files

---
+++ tailwind.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  content: ["./src/**/*.{html,js,jsx,ts,tsx}", "./html_clone/**/*.{html,js,jsx,ts,tsx}"], // Ensure it scans your files
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
